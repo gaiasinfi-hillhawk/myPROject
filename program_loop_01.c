e43e4158f39ce41bae9c452461c512459a36b5d0
gaiasinfi-hillhawk <gaiasinfi@gmail.com>
2026-05-20 14:22:23 +0000

    update code

--- program_loop_01.c
+++ program_loop_01.c
@@ -1,1 +1,13 @@
-print ('hello')
+/* print hello 10 times*/
+
+#include <stdio.h>
+int main (){
+
+    int i;
+
+    for(i=0; i<10; i++){
+        printf("%d hello\n", i+1);
+    }
+    
+    return 0;
+}
---
+++ program_loop_02.c
@@ -0,0 +1,12 @@
+/*print om namah shivay 8 times*/
+
+#include <stdio.h>
+int main(){
+    int i;
+
+    for(i=0; i<20; i++){
+        printf(" %d om namah shivay\n", i);
+        
+    }
+    return 0;
+}
